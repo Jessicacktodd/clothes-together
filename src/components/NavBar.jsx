d944c014766a03f014b17f3af746087dd182c70f
Jess Todd <jessicacktodd@gmail.com>
2024-10-15 18:57:42 +0100

    made nav bar fit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,6 @@ export const NavBar = () => {
     return (
         <div id="navbar-div-container">
             <nav>
-                <button onClick={() => navigate("/")}>Homepage</button> 
                 <button onClick={() => navigate("/WhoWeAre")}>Who we are</button>
                 <button onClick={() => navigate("/FindOutMoreBrands")}>Find out more (Brands)</button>
                 <button onClick={() => navigate("/FindOutMoreCharities")}>Find out more (Charities)</button>
