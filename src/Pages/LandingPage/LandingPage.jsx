c8c06e23db0bea709266846198e72ea2d2b78d91
Jess Todd <jessicacktodd@gmail.com>
2024-10-15 16:23:15 +0100

    js to jsx

---
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,10 @@
+import "./LandingPage.css"
+
+export const LandingPage = () => {
+    return (
+        <div className="landingPage">
+            <h1>Clothes Together</h1>
+            <Link to="/WhoWeAre">Who we are</Link>
+        </div>
+    );
+}
