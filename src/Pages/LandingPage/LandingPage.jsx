55e123ec608f156b66f7e2cf1e65e7d0ff69ce4d
Jess Todd <jessicacktodd@gmail.com>
2024-10-16 12:13:07 +0100

    blue and image added

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,5 @@
 import { NavBar } from "../../components/NavBar";
-import  clothes_together_logo  from "../../assets/clothes_together_logo.png";
-import instagram_logo from "../../assets/instagram_logo.png";
-import facebook_logo from "../../assets/facebook_logo.png"
+
 import moock_photo from "../../assets/mock_photo.png"
 import "./LandingPage.css"
 
@@ -9,29 +7,10 @@ export const LandingPage = () => {
     return (
         <>
         <div className="landingPage">
-            <header className="header">
-                <div className="social-links">
-                    <a href="https://www.instagram.com/pigeonorganics/" target="_blank" rel="noopener mnoreferrer">
-                        <img src={instagram_logo} alt="Instagram" className="social-icon" />
-                    </a>
-                    <a href="https://www.facebook.com/pigeonorganics/" target="_blank" rel="noopener mnoreferrer">
-                        <img src={facebook_logo} alt="Facebook" className="social-icon" />
-                    </a>
-                </div>
-                <NavBar />
-            </header>
-            <div class="header-section">
-                <img src={clothes_together_logo} alt="clothes together logo" className="clothes-together-logo"></img>
-                <nav class="spread-out-links">
-                    <a href="/WhoWeAre">Who we are</a>
-                    <a href="/FindOutMoreBrands">Find out more (Brands)</a>
-                    <a href="/FindOutMoreCharities">Find out more (Charities)</a>
-                </nav>
-            </div>
-
-            <section class="Nile-blue-section">
+            <NavBar />
+            <section className="Nile-blue-section">
                 <img src={moock_photo} alt="mock photo" className="section-image"></img>
-                <div class="text-block">
+                <div className="text-block">
                     <h2>Heading Text</h2>
                     <p>This is a chunk of text describing something related to the image or section. You can add a paragraph here, explain things, or even list things as necessary.</p>
                 </div>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,43 @@
 import { useNavigate } from "react-router-dom";
 import "./NavBar.css"
+import  clothes_together_logo  from "../assets/clothes_together_logo.png";
+import instagram_logo from "../assets/instagram_logo.png";
+import facebook_logo from "../assets/facebook_logo.png"
 
 export const NavBar = () => {
     const navigate = useNavigate();
 
     return (
         <div id="navbar-div-container">
-            <nav>
-                <button onClick={() => navigate("/WhoWeAre")}>Who we are</button>
-                <button onClick={() => navigate("/FindOutMoreBrands")}>Find out more (Brands)</button>
-                <button onClick={() => navigate("/FindOutMoreCharities")}>Find out more (Charities)</button>
-            </nav>
+            <header className="header">
+                <div className="social-links">
+                    <a href="https://www.instagram.com/pigeonorganics/" target="_blank" rel="noopener mnoreferrer">
+                        <img src={instagram_logo} alt="Instagram" className="social-icon" />
+                    </a>
+                    <a href="https://www.facebook.com/pigeonorganics/" target="_blank" rel="noopener mnoreferrer">
+                        <img src={facebook_logo} alt="Facebook" className="social-icon" />
+                    </a>
+                </div>
+                
+                <div className="right-side-links">
+                    <nav>
+                        {/* <button onClick={() => navigate("/WhoWeAre")}>Who we are</button> */}
+                        <button onClick={() => navigate("/FindOutMoreBrands")}>Find out more (Brands)</button>
+                        <button onClick={() => navigate("/FindOutMoreCharities")}>Find out more (Charities)</button>
+                    </nav>
+                </div>
+            </header>
+           
+            <div className="header-section">
+                <img src={clothes_together_logo} alt="clothes together logo" className="clothes-together-logo"></img>
+                
+                <nav className="spread-out-links">
+                    <a href="/WhoWeAre">Who we are</a>
+                    <a href="/FindOutMoreBrands">Find out more (Brands)</a>
+                    <a href="/FindOutMoreCharities">Find out more (Charities)</a>
+                </nav>
+            </div>
+           
         </div>
     );
 };
